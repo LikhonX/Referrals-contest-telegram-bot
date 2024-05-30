58057655054bded248b60253e34d454e5c820988
LikhonX <likhoncryptocalls@gmail.com>
2024-05-31 05:45:13 +0600

    Update config.js

--- core/config.js
+++ core/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-  bot_token: 'Telegram BOT token',
-  admin: 'Telegram admins here',
-  tweet_id: 'Your Tweet ID',
+  bot_token: '6629572600:AAFXMiGg9p4URAXnCi-w2EjEePPnUGvmYRg',
+  admin: '5904662394',
+  tweet_id: 'likhonscripts',
   mongoLink:
-    'Your mongo DB cluester',
+    'mongodb+srv://likhon:VX6RtTdMQCiMFZKX@cluster0.jlbf79x.mongodb.net/',
 };
